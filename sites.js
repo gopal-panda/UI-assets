fd7a4c3c13a771352b818c68d128ad0f416c7074
gopal-panda <87986034+gopal-panda@users.noreply.github.com>
2024-11-25 12:49:08 -0600

    Update sites.js

--- sites.js
+++ sites.js
@@ -1,6 +1,6 @@
 var sites = {
   "QFC" : {
     "banner" : "https://cdn.jsdelivr.net/gh/BalaGanaparthi/cswg@1.0.3/Icons/QFC_Banner@2x.png",
-    "background" "https://cdn.jsdelivr.net/gh/gopal-panda/UI-assets@0.0.1/QFC_Icon_Background_right.svg"
+    "background" : "https://cdn.jsdelivr.net/gh/gopal-panda/UI-assets@0.0.1/QFC_Icon_Background_right.svg",
   }
 }
